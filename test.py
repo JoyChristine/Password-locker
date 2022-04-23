f2a426e2901e34540c9e00203dd4a665f2f99fb3
Joy Christine <joychristin2@gmail.com>
2022-04-23 15:17:23 +0300

    import modules

--- test.py
+++ test.py
@@ -1,1 +1,17 @@
 # Tests
+import unittest
+import user
+
+class TestUser(unittest.TestCase):
+
+    def setUp(self):
+        self.new_user = user("James","Muriuki","abcd")
+
+    def test_init(self):
+        self.assertEqual(self.new_user.fname == "James")
+        self.assertEqual(self.new_user.lname == "Muriuki")
+        self.assertEqual(self.new_user.password == "abcd") 
+
+
+if __name__ == "__main__":
+    unittest.main()
