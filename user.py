98e2aa3b1aeb4a5970da3040b44ca724cae88bfd
Joy Christine <joychristin2@gmail.com>
2022-04-23 20:15:42 +0300

    add class Credentials

--- user.py
+++ user.py
@@ -16,6 +16,28 @@ class User:
     # every item is printed is put in the dict
     def save_user(self):
         User.userslist.append(self)
+    
+
+    # delete
+    def delete_user(self):
+        User.userslist.remove(self)
+
+    # display users
+    @classmethod 
+    def display_user(cls):
+        return cls.userslist
+
+    @classmethod
+    def find_by_number(cls,number):
+        for user in cls.userslist:
+            if user.password == number:
+               return user
+
+    @classmethod
+    def find_user(cls,number):
+        for user in cls.userslist:
+            if user.username == number:
+                return user
 
 class Credentials:
 
@@ -28,3 +50,19 @@ class Credentials:
 
     def save_account(self):
         Credentials.accounts.append(self)
+    
+    # delete
+    def delete_account(self):
+        Credentials.accounts.remove(self)
+
+    # display account
+    @classmethod
+    def display_account(cls):
+        # for account in cls.accounts:
+            return cls
+
+    @classmethod
+    def find_by_number(cls,number):
+        for account in cls.accounts:
+            if account.accountusername == number:
+                return True
