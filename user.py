fa9d1e496aecd5e441ca673e3a3035e3eb7673f2
Joy Christine <joychristin2@gmail.com>
2022-04-23 15:20:42 +0300

    create class user & credentials

--- user.py
+++ user.py
@@ -1,6 +1,25 @@
 # class of user
 class User:
 
-    def __init__(self,name,password):
-        self.name = name
+
+    userslist=[]
+    def __init__(self,fname,lname,password):
+        self.fname = fname
+        self.lname = lname
         self.password = password
+
+    # every item is printed is put in the dict
+    def save_user(self):
+        User.userslist.append(self)
+
+class Credentials:
+
+    accounts =[]
+
+    def __init__(self,accountusername,accountname,accountpassword):
+        self.accountusername = accountusername
+        self.accountname = accountname
+        self.accountpassword = accountpassword
+
+    def save_account(self):
+        Credentials.accounts.append(self)
