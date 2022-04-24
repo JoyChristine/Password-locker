74d0b6f777a5825000cf6afc5ed3ceb44af4cfd3
Joy Christine <joychristin2@gmail.com>
2022-04-24 21:37:10 +0300

    create credentials class & add class method to display all accounts

---
+++ credentials.py
@@ -0,0 +1,53 @@
+class Credentials:
+    """
+    Credentials class to create accounts for users
+    """
+    credentials_list = []
+
+    def __init__(self, accountname, accountpassword):
+        """
+        Method that defines the properties of each object
+        """
+        self.accountname = accountname
+        self.accountpassword = accountpassword
+
+  
+
+
+    def save_account(self):
+        """
+        Function to save the new objects created in this case -accounts
+        """
+        self.credentials_list.append(self)
+
+    def delete_account(self):
+        """
+        Function to delete the accounts saved
+        """
+        Credentials.credentials_list.remove(self)
+
+    @classmethod
+    def find_account_by_name(cls, accountname):
+        """
+        class method that takes in an account name and returns it if found
+        """
+        for credential in cls.credentials_list:
+            if credential.accountname == accountname:
+                return credential
+
+    @classmethod
+    def credential_exists(cls, name):
+        """
+        class method to check if a credential exists
+        """
+        for credential in cls.credentials_list:
+            if credential.accountname == name:
+                return True
+        return False
+
+    @classmethod
+    def display_account(cls):
+        """
+        Class method to display all accounts saved
+        """
+        return cls.credentials_list
